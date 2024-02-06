4f1c8205c2f52e84a2db078699eea89877d49f65
Paulo <git@pauloo27.dev>
2024-02-06 09:11:04 -0300

    feat: support status filter

--- rest/api/orders/list.ts
+++ rest/api/orders/list.ts
@@ -2,12 +2,13 @@ import 'reflect-metadata';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { newDataSource } from '../../core/db/datasource';
 import { newHandler } from '../../core/api/handler';
-import { Order } from '../../entities/order';
+import { Order, OrderStatus } from '../../entities/order';
 
 const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const ds = await newDataSource();
     const repo = ds.getRepository(Order);
-    const orders = await repo.find({ order: { id: 'ASC' } });
+    const statusFilter = event.queryStringParameters?.status as OrderStatus;
+    const orders = await repo.find({ order: { id: 'ASC'}, where: { status: statusFilter }});
 
     return {
         statusCode: 200,
